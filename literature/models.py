eda5cc191ac394fd4323c870f23b95f3182ed519
SpaceFox <ujueseo.yeou@gmail.com>
2014-10-25 00:00:01 +0200

    MAJ modèle (infos complémentaires)

--- literature/models.py
+++ literature/models.py
@@ -2,6 +2,8 @@
 
 from django.db import models
 import markdown
+from django.template.defaultfilters import slugify
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -11,18 +13,19 @@ class Element(models.Model):
     """A text element"""
 
     title = models.CharField(u'Titre', max_length=50)
-    slug = models.SlugField(u'Slug', unique=True)
+    slug = models.SlugField(u'Slug', unique=True, editable=False)
     pub_date = models.DateTimeField(u'Date de publication', auto_now_add=True)
     last_update = models.DateTimeField(u'Date de dernière mise à jour', auto_now=True)
     preface = models.TextField(u'Préface', null=True, blank=True)
-    preface_html = models.TextField(u'Préface (HTML)', null=True, blank=True)
+    preface_html = models.TextField(u'Préface (HTML)', null=True, blank=True, editable=False)
     postface = models.TextField(u'Postface', null=True, blank=True)
-    postface_html = models.TextField(u'Postface (HTML)', null=True, blank=True)
+    postface_html = models.TextField(u'Postface (HTML)', null=True, blank=True, editable=False)
 
-    def save(self, *args, **kwargs):
+    def save(self):
         self.preface_html = markdown.markdown(self.preface)
         self.postface_html = markdown.markdown(self.postface)
-        super(Element, self).save(*args, **kwargs)
+        self.slug = slugify(self.title)
+        super(Element, self).save()
 
     class Meta:
         abstract = True
@@ -33,7 +36,7 @@ class Book(Element):
     """A book, with one or more chapters"""
 
     def __unicode__(self):
-        return self.title
+        return u'Livre {}'.format(self.title)
 
 
 class Chapter(Element):
@@ -42,8 +45,14 @@ class Chapter(Element):
 
     book = models.ForeignKey(u'Book')
     text = models.TextField(u'Texte')
-    text_html = models.TextField(u'Texte (HTML)', blank=True)
+    text_html = models.TextField(u'Texte (HTML)', blank=True, editable=False)
 
     def save(self, *args, **kwargs):
         self.text_html = markdown.markdown(self.text)
         super(Chapter, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return u'Chapitre {}'.format(self.title)
+
+    def get_absolute_url(self):
+        return reverse(u'literature.views.chapter', kwargs={u'slug': self.slug})
