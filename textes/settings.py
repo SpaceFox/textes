f0b788db7296237918937ab5f25ce8eb77eb37a2
SpaceFox <ujueseo.yeou@gmail.com>
2014-10-24 19:53:37 +0200

    Paramétrage de l'application

--- textes/settings.py
+++ textes/settings.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 """
 Django settings for textes project.
 
@@ -66,7 +68,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
